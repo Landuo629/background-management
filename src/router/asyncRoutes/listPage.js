316fd6e30712c3db9daf2b53c2b7a48032a1fdae
landuo <847158759@qq.com>
2022-06-20 16:37:08 +0800

    动态路由添加文件夹

---
+++ src/router/asyncRoutes/listPage.js
@@ -0,0 +1,31 @@
+// import Layout from '@/layout'
+const Layout = () => import('@/layout')
+export default [{
+  path: '/listPage',
+  component: Layout,
+  redirect: '/listPage',
+  name: 'listPage',
+  children: [{
+    path: '/listPage',
+    name: 'listPage',
+    component: () => import('@/views/listPage/index'),
+    meta: {
+      title: '列表',
+      icon: 'orderManage',
+      roles: ['superAdmin', 'admin']
+    }
+  },
+  {
+    path: '/listPageDetail',
+    name: 'listPageDetail',
+    component: () => import('@/views/listPage/detail'),
+    meta: {
+      title: '列表详情页',
+      icon: '',
+      roles: ['superAdmin', 'admin']
+    },
+    hidden: true
+  }
+  ]
+}
+]
